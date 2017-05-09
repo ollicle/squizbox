f3340059504df03a1b70c35f61aa4776d21bae27
Oliver Boermans <boermans+github@gmail.com>
2017-05-09 20:59:21 +0930

    Clear document contents

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // import 'babel-polyfill';
 import Squizbox from './squizbox';
 
+document.body.innerHTML = '';
+
 const squiz = new Squizbox({
   // `target` is the only required option – the element
   // to render the component to
-  target: document.querySelector( 'main' ),
+  target: document.querySelector( 'body' ),
 
   // `data` is optional. A component can also have
   // default data – we'll learn about that later.
