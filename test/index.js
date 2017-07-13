3e0231953327a304528fa5ae3f81a6f9fd64d19c
Oliver Boermans <ollie@ollicle.com>
2017-07-13 21:46:54 +0930

    New: inject button to exit test, pass test url as arg

--- test/index.js
+++ test/index.js
@@ -1,15 +1,25 @@
 var webdriverio = require('webdriverio');
 var options = { desiredCapabilities: { browserName: 'firefox', marionette: true } };
-var client = webdriverio.remote(options);
-client
+var browser = webdriverio.remote(options);
+var url = process.argv[2];
+
+browser
     .init()
-    .url('http://matanich.com/')
-    .getTitle().then(function(title) {
-        console.log('Title is: ' + title);
-    })
-    .execute(function(){
+    .url(url)
+    .executeAsync(function(done){
+    
+    	//	Load script to test
 		var script = document.createElement('script');
 		script.src = 'http://192.168.178.26:3333/build/index.js';
 		document.head.appendChild(script);
-    });
-//     .end();
+		
+		//	Insert test exit button
+        setTimeout(function() {
+			var exit = document.createElement('button');
+			exit.textContent = "Exit test";
+			exit.addEventListener('click', function (){
+				done();
+			}, false);
+            document.body.insertBefore(exit, document.body.firstChild);
+        }, 30, done);
+    }).end();
