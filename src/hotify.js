a9a3f347900162e7cf0661a062d2268ecbed0fa7
Oliver Boermans <ollie@ollicle.com>
2017-07-14 20:38:49 +0930

    Give up on hot reloading for now

--- src/hotify.js
+++
@@ -1,5 +0,0 @@
-export default = function (instance, options, Component) {
-  var data = instance.get();
-  instance.teardown();
-  return new Component(Object.assign({}, options, { data: data }));
-};
